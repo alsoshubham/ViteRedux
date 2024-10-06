c84627b9c0b5deac28a32d34f6f71cbfabeed4dd
alsoshubham <shubhamsahu100100@hotmail.com>
2024-10-06 14:15:48 +0530

    end

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ const  App = () => {
     counter = counter -1;
     setCounter(counter)
   }
-
+  
   return (
     // <div><Counter/></div>
     <>
     <div className="flex flex-col justify-center items-center">
       <h1 className="justify-center item-center font-bold text-black text-2xl">Counter App</h1>
-      <h2 className="text-black">counter value: {counter}</h2>
+      <h2 className="text-black text-2xl">counter value: {counter}</h2>
       <div className="p-10 mt-10 gap-5">
-      <button className="bg-blue-400 hover:bg-violet-400 text-white font-bold py-2 px-4 rounded" onClick={addValue}
-      >Increment</button>
+      <button 
+      className="bg-blue-400 hover:bg-violet-400 text-white font-bold py-2 px-4 rounded" onClick={addValue}>Increment</button>
       <p className="flex justify-center py-2 px-4">{counter}</p>
       <button className="bg-blue-400 hover:bg-violet-400 text-white font-bold py-2 px-4 rounded" onClick={subValue}>Decrement</button>
       </div>
